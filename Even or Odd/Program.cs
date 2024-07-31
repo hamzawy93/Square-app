3a177f096fe849bcc8ecb7d90a2db1098449b53b
darkk <darkk@HAMZAWY>
2024-07-31 07:35:38 +0300

    Add project files.

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter A Number");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine("The Number IS Even");
+else Console.WriteLine("The Number Is Odd");
+
---
+++ Square app/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter A The First Side");
+int Sideone = int.Parse(Console.ReadLine());
+Console.WriteLine("Side one is "+ Sideone);
+Console.WriteLine("Enter A The Second Side");
+int Sidetwo = int.Parse(Console.ReadLine());
+Console.WriteLine("Side Two is "+ Sidetwo);
+if (Sideone == Sidetwo)
+    Console.WriteLine("The Shape is Square");
+else Console.WriteLine("The Shape Is Rectangle");
+
